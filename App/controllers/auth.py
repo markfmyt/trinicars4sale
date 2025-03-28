ca92792e115a47570e10c6c3150f4f4aaf985bb6
Mark <74921616+markfmyt@users.noreply.github.com>
2025-03-27 20:28:46 -0400

    Update auth.py

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,5 +1,4 @@
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt_identity, verify_jwt_in_request
-
 from App.models import User
 
 def login(username, password):
